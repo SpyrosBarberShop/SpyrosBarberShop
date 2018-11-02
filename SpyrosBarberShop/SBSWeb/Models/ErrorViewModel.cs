adc2dcb5333c3df698620a78e85eea5afb658260
n.vourdoumpas <n.vourdoumpas@neurocom.gr>
2018-11-02 14:26:13 +0200

    Implemented new asp.net core project

---
+++ SpyrosBarberShop/SBSWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SBSWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
